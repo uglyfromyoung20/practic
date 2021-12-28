57e289950ef2698d1bbb9c3050ba133429c41d0a
altyshkin <29455komandor@mail.ru>
2021-12-29 02:42:17 +0400

    5.4

---
+++ src/main/java/ru/ssau/tk/ugly/practic/Location.java
@@ -0,0 +1,55 @@
+package ru.ssau.tk.ugly.practic;
+
+import java.util.Objects;
+
+public class Location {
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return id == location.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
